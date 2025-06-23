1da28c87456379d6df882cbebdd1d7b37ab2d5fe
2Diego2 <diegoabelleyra74@gmail.com>
2025-06-23 12:22:00 -0300

    1-Realizamos una BD ficticia. 2-Realizamos en eventos.js un recorrido de la BD para tomar informacion. 3-Se llego a realizar que tome en cuenta el dia de hoy y diga que Empleado cumple su aniversario en 3 dias.

--- proyecto-practicas/src/eventos.js
+++ proyecto-practicas/src/eventos.js
@@ -1,2 +1,60 @@
 /*Define y exporta los eventos personalizados usando 
 EventEmitter. Aquí puedes manejar lo que ocurre cuando se detecta un aniversario */
+
+
+const fs = require("fs");
+const path = require("path");
+const dayjs = require("dayjs");
+const  EventEmitter = require("events");
+
+
+
+const ruta = path.join(__dirname, "../data/trabajadores.json");
+
+
+const personas = fs.readFileSync(ruta,"utf-8");
+let trabajadores = JSON.parse(personas);
+
+
+
+function AniversarioPersonas(trabajadores){
+ const hoy=dayjs();
+ const enTresDias = hoy.add(3,"day");
+
+ //const empleados= [];
+for (let i=0; i<trabajadores.length; i++){
+    const trabajador = trabajadores[i];
+    const { nombre, fechaEntrada, mail } = trabajador;
+  console.log(nombre, fechaEntrada, mail);
+ const fechaIngreso = dayjs(fechaEntrada); //Sirve para cambiar de String a Fecha :)
+
+
+let fechaAniversario = fechaIngreso.year(enTresDias.year());
+
+if (fechaAniversario = enTresDias){
+    console.log(`El trabajador ${nombre} cumplira su aniversario en 3 dias. `);
+}
+
+
+
+console.log(fechaAniversario);
+ 
+
+}
+
+};
+
+AniversarioPersonas(trabajadores);
+
+
+/*
+aniversarioPersona.on("aniversario",(fechaEntrada) => {
+console.log(`El trabajador: ${nombre} cumplira en 3 dias su aniversario numero: ${aniversarioPersona}`);
+
+});
+
+aniversarioPersona.emit("aniversario", trabajadores.fechaEntrada);
+
+//Llamar de la BD la fecha de ingreso
+
+*/
