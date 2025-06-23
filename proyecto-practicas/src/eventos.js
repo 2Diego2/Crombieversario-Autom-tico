4a889dd2a4dc5e40997528690d2f1062a682153d
2Diego2 <diegoabelleyra74@gmail.com>
2025-06-23 09:30:03 -0300

    Primer commit

---
+++ proyecto-practicas/src/eventos.js
@@ -0,0 +1,2 @@
+/*Define y exporta los eventos personalizados usando 
+EventEmitter. Aquí puedes manejar lo que ocurre cuando se detecta un aniversario */
---
+++ proyecto-practicas/src/index.js
@@ -0,0 +1,2 @@
+/*3. index.js
+Es el archivo principal. Lee los datos, usa las utilidades y emite eventos según la lógica. */
